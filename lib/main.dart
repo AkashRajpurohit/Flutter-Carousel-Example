84397d0b6c7dbc59138afd44cce6bc67e0d4d3e6
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-07 11:08:32 +0530

    :art: debugbanner removed; themedata changed

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
       home: HomePage(),
     );
