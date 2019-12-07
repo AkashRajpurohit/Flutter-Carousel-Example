4ba7a52675b31fc283b9f64cd201e8c50d731b77
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-07 10:16:34 +0530

    :art: next and prev image logic

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  int photoIndex = 0;
+  List<String> photos = [
+    'http://lorempixel.com/640/480/food',
+    'http://lorempixel.com/640/480/abstract',
+    'http://lorempixel.com/640/480/nature',
+    'http://lorempixel.com/640/480/fashion'
+  ];
+
+  void _prevImage() {
+    setState(() => photoIndex = photoIndex > 0 ? photoIndex - 1 : 0);
+  }
+
+  void _nextImage() {
+    setState(() => photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
