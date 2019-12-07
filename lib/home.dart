3982d952ed1326a4875cac197fb47abee40d0fe0
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2019-12-07 10:54:40 +0530

    :art: images changed; show container

--- lib/home.dart
+++ lib/home.dart
@@ -9,10 +9,10 @@ class _HomePageState extends State<HomePage> {
 
   int photoIndex = 0;
   List<String> photos = [
-    'http://lorempixel.com/640/480/food',
-    'http://lorempixel.com/640/480/abstract',
-    'http://lorempixel.com/640/480/nature',
-    'http://lorempixel.com/640/480/fashion'
+    'assets/1.jpg',
+    'assets/2.jpg',
+    'assets/3.jpg',
+    'assets/4.jpg'
   ];
 
   void _prevImage() {
@@ -20,13 +20,58 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _nextImage() {
-    setState(() => photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex);
+    setState(() => photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carousel Example"),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    image: DecorationImage(
+                      image: AssetImage(photos[photoIndex]),
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                  width: 300.0,
+                  height: 400.0,
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: _prevImage,
+                color: Colors.red,
+                iconSize: 40.0,
+              ),
+              SizedBox(width: 20.0),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: _nextImage,
+                color: Colors.green,
+                iconSize: 40.0,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
